54ef34dfa8d9164e99fe9e3c1f89202b054e50f1
Michelle <michelleutomi@gmail.com>
2022-06-23 13:38:21 +0100

    initial commit

---
+++ intro.js
@@ -0,0 +1,7 @@
+let fullName = 'Name: Michelle';
+let height = 'Height: 5ft3';
+let country = 'Country: Nigeria';
+
+// alert(`${fullName height country}`);
+
+console.log(`${fullName} \n ${height} \n ${country}`);
