8a1530115288985b610fdeba6d7e66bc9273df41
Michelle Utomi <michelleutomi@yahoo.com>
2022-06-23 14:41:15 +0100

    Update intro.js

--- intro.js
+++ intro.js
@@ -2,6 +2,5 @@ let fullName = 'Name: Michelle';
 let height = 'Height: 5ft3';
 let country = 'Country: Nigeria';
 
-// alert(`${fullName height country}`);
 
-console.log(`${fullName} \n ${height} \n ${country}`);
+alert(`${fullName} \n ${height} \n ${country}`);
